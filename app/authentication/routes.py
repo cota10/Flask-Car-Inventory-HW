72703394c0b6da14c7d6043b1cb28922fe5ca633
Ulises Cota <ulisescota55@gmail.com>
2022-12-14 19:36:39 -0800

    second commit

---
+++ app/authentication/routes.py
@@ -0,0 +1,30 @@
+from forms import UserLoginForm
+from models import User, db
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+
+auth = Blueprint('auth', __name__, template_folder='auth_template')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+            user = User(email, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+
+
+
+            flash(f'You have successfully created a user account {email}', 'User-created')
+            return redirect(url_for('site.home'))
+    except:
+        raise Exception('Invalid form data: Please check your form')
+    return render_template('sign_up.html', form=form)
+
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
  
 
@@ -12,4 +13,4 @@ class Config():
     """
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from the sqlalchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
