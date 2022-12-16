27349af150ecd3b92800d563410f082882ef8210
Ulises Cota <ulisescota55@gmail.com>
2022-12-16 10:58:29 -0800

    hw #3

--- models.py
+++ models.py
@@ -1,13 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import uuid
+import uuid 
 from datetime import datetime
-import secrets
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from flask_login import LoginManager
+from flask_marshmallow import Marshmallow 
+import secrets
 
+login_manager = LoginManager()
+ma = Marshmallow()
 db = SQLAlchemy()
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key=True)
     first_name = db.Column(db.String(150), nullable=True, default='')
     last_name = db.Column(db.String(150), nullable = True, default = '')
@@ -38,3 +47,33 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been added to the database'
+
+class Car(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    year = db.Column(db.String(150), nullable = False)
+    make = db.Column(db.String(200))
+    model = db.Column(db.String(20))
+    color = db.Column(db.String(200))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+    def __init__(self,year,make,model,color,user_token, id = ''):
+        self.id = self.set_id()
+        self.year = year
+        self.make = make
+        self.model = model
+        self.color = color
+        self.user_token = user_token
+
+
+    def __repr__(self):
+        return f'The following car has been added to the Car Inentory: {self.name}'
+
+    def set_id(self):
+        return (secrets.token_urlsafe())
+
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'year','make','model', 'color']
+
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
